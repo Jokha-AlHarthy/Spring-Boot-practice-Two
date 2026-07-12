4d7cf54c3925fc844a0b0c3d039a5c1039bc027e
Jokha-AlHarthy <jokha.hamed.official@gmail.com>
2026-07-12 16:46:34 +0400

    Initial project

---
+++ src/EvenNumberAnalyzer.java
@@ -0,0 +1,10 @@
+public class EvenNumberAnalyzer {
+    public static void main(String [] args){
+        int limit = 20;
+        int count = 0;
+        int sum = 0;
+        for(int i=1; ){
+
+        }
+    }
+}
