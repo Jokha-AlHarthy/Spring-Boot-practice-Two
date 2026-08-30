c2b712b4c21a3b0a556235addd4635e22f0553ce
Jokha-AlHarthy <jokha.hamed.official@gmail.com>
2026-08-30 15:28:35 +0400

    feat(entities): Updating the DemoApplication.java file

--- demo/src/main/java/com/example/demo/entites/BaseClass.java
+++ demo/src/main/java/com/example/demo/entites/BaseClass.java
@@ -1,4 +1,16 @@
 package com.example.demo.entites;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Getter
+@Setter
 public class BaseClass {
+    private UUID id;
+    private Boolean isActive;
+    private Date createdDate;
+    private Date updatedDate;
 }
