8a4e0c122c200bfc0f8aaf8c4129ed07b9df6c7b
Jokha-AlHarthy <jokha.hamed.official@gmail.com>
2026-08-30 15:27:35 +0400

    feat(demo): Updating the DemoApplication.java file

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,11 +1,15 @@
 package com.example.demo;
 
+import com.example.demo.entites.Pet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootApplication
 public class DemoApplication {
-
+	public static List<Pet> Pet_List = new ArrayList<>();
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
